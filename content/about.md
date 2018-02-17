019871317278c14b572cfe16b160a979d5609875
Kirsten Dodd <kirsten.jvd@gmail.com>
2018-02-16 20:30:32 -0800

    First commit of hugo build for Aslan

---
+++ content/about.md
@@ -0,0 +1,62 @@
+---
+title: About Us
+slug: about
+description: something
+BgImg: tout_bg.jpg
+blurb: Playing in the yard since 2007
+---
+<!-- 
+{{< figure src="/images/tout_bg.jpg" title="Photo by Ales Krivec on Unsplash" >}} -->
+
+Inspired by the breath-taking natural beauty of Vancouver Island, Aslan was founded over ten years ago by Chris and his love for creativity and outdoor space. Since then, a passionate group of people have joined him in his adventure of making every yard an enchanting space for every client.
+
+## We live by our values
+Each day at Aslan is guided by the same principles of giving exceptional customer service, keeping our promises, and continually innovating. We are solid on our word and we believe that every project deserves a creative solution.
+
+## This is us
+We are Aslan, and we want to tackle any challenge as an opportunity for improving ourselves and reinventing our craft. We take our inspiration from the magnificent scenery that surrounds us, and we want to cultivate a genuinely West Coast flavour in each of our projects. We are innovative, creative, and hard working.
+Does this sound like a good fit for you? Get your [free consultation and estimate] (http://example.com/ "Free Consultation and Estimate").
+
+
+{{< figure src="/images/chris.png" title="Photo of Chris" >}}
+
+### Chris
+
+CEO and owner of Aslan, Chris is everywhere and working on every project all at the same time. Although this is not his original career path, he likes to say that landscaping found him. With his appreciation for art and beauty, he gets to use his creative mind everyday in designing beautiful landscapes and outdoor living spaces for his clients. As for spending all this time outside enjoying the amazing West Coast climate? It’s just a bonus.
+
+{{< figure src="/images/hannah.png" title="Photo of Hannah" >}}
+
+### Hannah
+
+Acting both as a CEO support and as the office manager, Hannah has been working alongside Chris for a long time and knows everything about the landscaping business. Kind and knowledgeable, she is the person to go to for anything regarding your project, from arranging a consultation to giving you the latest news.
+
+{{< figure src="/images/Laura.png" title="Photo of laura" >}}
+
+### Laura
+
+Laura brings a veteran presence to the team and years of experience in the landscape and horticulture world. Her experience in golf course and city planning/maintenance gives her the ability to manage our maintenance division with the utmost of care and attention. Laura is certified in many areas and has a diploma of horticulture from the university of Guelph.
+
+{{< figure src="/images/angie.png" title="Photo of Angie" >}}
+
+### Angie
+
+Passionate and reliable, Angie has a true gift for creating exceptional landscapes. With her extraordinary leadership and organizational skills, she was the natural choice for leading one of our maintenance crews and ensuring the best service to our clients. 
+
+{{< figure src="/images/andrew.png" title="Photo of Andrew" >}}
+
+### Andrew
+
+Hardworking and dedicated, Andrew loves the satisfaction of a job well done. Taking his work ethic from his upbringing on a farm, he has the greatest mix of expertise and muscle to tackle any landscaping job. He has been with us since 2001, and we are always impressed by his knowledge and expertise.
+
+{{< figure src="/images/dan.png" title="Photo of Dan" >}}
+
+### Dan
+
+Dan came to us with a mixed bag of expertise in the industrial world, backed by a mechanical mind. His intense attention to detail and commitment to the company and client bring great value to all of our projects. Besides being a welder, inspiring mechanic and all around gear-head Dan is also a certified irrigation technician.
+
+{{< figure src="/images/tysen.png" title="Photo of Tysen" >}}
+
+### Tysen
+
+Tysen came to us from Bear Mountain where he worked on the landscape maintenance crew. He assumed the role of crew chief and continues to exercise his ability to learn and grow in the industry. His contagious smile and warm soul are sure to win you over.
+
---
+++ gulpfile.js
@@ -0,0 +1,150 @@
+/**
+ * Gulpfile.
+ *
+ * Implements:
+ *      1. Live reloads browser with BrowserSync.
+ *      2. CSS: Sass to CSS conversion, error catching, Autoprefixing, Sourcemaps,
+ *         CSS minification, and Merge Media Queries.
+ *      3. JS: Concatenates & uglifies Vendor and Custom JS files.
+ *      4. Images: Minifies PNG, JPEG, GIF and SVG images.
+ *      5. Watches files for changes in CSS or JS.
+ *      6. Watches files for changes in HTML.
+ *      7. Corrects the line endings.
+ *      8. InjectCSS instead of browser page reload.
+ *
+ * @author Kirsten Dodd (@kirstenjvd)
+ */
+
+// Project related.
+var project                 = 'aslan'; // Project Name.
+var projectURL              = 'http://localhost:1313/';
+
+// JS Vendor related.
+var jsVendorSRC             = 'src/js/vendor/*.js'; // Path to JS vendor folder.
+var jsVendorDestination     = 'static/js'; // Path to place the compiled JS vendors file.
+var jsVendorFile            = 'vendors'; // Compiled JS vendors file name.
+// Default set to vendors i.e. vendors.js.
+
+// JS Custom related.
+var jsCustomSRC             = 'src/js/*.js'; // Path to JS custom scripts folder.
+var jsCustomDestination     = 'static/js'; // Path to place the compiled JS custom scripts file.
+var jsCustomFile            = 'custom'; // Compiled JS custom file name.
+// Default set to custom i.e. custom.js.
+
+// Images related.
+var imagesSRC               = 'src/images/*.{png,jpg,gif,svg}'; // Source folder of images which should be optimized.
+var imagesDestination       = 'static/images/'; // Destination folder of optimized images. Must be different from the imagesSRC folder.
+
+
+
+var gulp         = require("gulp");
+
+// CSS related plugins.
+var sass         = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    mmq          = require('gulp-merge-media-queries'),
+    csslint      = require('gulp-csslint');
+
+// JS related plugins.
+var concat       = require('gulp-concat'), // Concatenates JS files
+    uglify       = require('gulp-uglify'), // Minifies JS files
+    jshint       = require('gulp-jshint');
+
+// Image realted plugins.
+var imagemin     = require('gulp-imagemin');
+
+// Utility related plugins.
+var del          = require("del"),
+    hash         = require("gulp-hash"),
+    lineec       = require('gulp-line-ending-corrector'),
+    notify       = require('gulp-notify'), 
+    browserSync  = require('browser-sync').create(), 
+    reload       = browserSync.reload, 
+    del          = require("del"),
+    hash         = require("gulp-hash");
+
+gulp.task( 'browser-sync', function() {
+  browserSync.init( {
+
+    // Project URL.
+    proxy: projectURL,
+    open: true,
+    injectChanges: true,
+
+  } );
+});
+
+// Compile SCSS files to CSS
+gulp.task("scss", function () {
+  del(["static/css/**/*"])
+  gulp.src("src/scss/**/*.scss")
+    .pipe(sass({outputStyle : "compressed"}))
+    .pipe(autoprefixer({browsers : ["last 20 versions"]}))
+    // .pipe(csslint())
+    // .pipe(csslint.formatter())
+    .pipe(hash())
+    .pipe( mmq( { log: true } ) )
+    .pipe( lineec() )
+    .pipe(gulp.dest("static/css"))
+    .pipe(hash.manifest("hash.json"))
+    .pipe(gulp.dest("data/css"))
+    .pipe( browserSync.stream() ) 
+    .pipe( notify( { message: 'TASK: "styles" Completed! 💯', onLast: true } ) )
+})
+
+// Hash images
+gulp.task("images", function () {
+  del([imagesDestination])
+  gulp.src(imagesSRC)
+    .pipe( imagemin( {
+      progressive: true,
+      optimizationLevel: 3,
+      interlaced: true,
+      svgoPlugins: [{removeViewBox: false}]
+    } ) )
+    .pipe(gulp.dest(imagesDestination))
+    .pipe(gulp.dest("data/images"))
+    .pipe( notify( { message: 'TASK: "images" Completed! 💯', onLast: true } ) );
+
+})
+
+// Hash javascript
+gulp.task( 'vendorsJs', function() {
+  del([jsVendorDestination])
+  gulp.src( jsVendorSRC )
+    .pipe( concat( jsVendorFile + '.js' ) )
+    .pipe( lineec() ) 
+    .pipe(hash())
+    .pipe( gulp.dest( jsVendorDestination ) )
+    .pipe( uglify() )
+    .pipe( lineec() ) 
+    .pipe(hash.manifest("hash.json"))
+    .pipe( gulp.dest( jsVendorDestination ) )
+    .pipe( notify( { message: 'TASK: "vendorsJs" Completed! 💯', onLast: true } ) );
+ });
+
+ gulp.task( 'customJS', function() {
+    del([jsCustomDestination])
+    gulp.src( jsCustomSRC )
+    .pipe( concat( jsCustomFile + '.js' ) )
+    .pipe( lineec() ) 
+    .pipe(hash())
+    .pipe( gulp.dest( jsCustomDestination ) )
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', { verbose: true }))
+    .pipe(jshint.reporter('fail'))
+    .pipe( uglify() )
+    .pipe( lineec() )
+    .pipe(hash.manifest("hash.json"))
+    .pipe( gulp.dest( jsCustomDestination ) )
+    .pipe( notify( { message: 'TASK: "customJs" Completed! 💯', onLast: true } ) );
+ });
+
+// Watch asset folder for changes
+gulp.task("default", ["scss", "images", "vendorsJs", "customJS", "browser-sync"], function () {
+  gulp.watch("src/scss/**/*", ["scss"])
+  gulp.watch("src/images/**/*", ["images"])
+  gulp.watch( "layouts/**/*.html", [ reload ] );
+  gulp.watch( jsVendorSRC, [ 'vendorsJs', reload ] );
+  gulp.watch( jsCustomSRC, [ 'customJS', reload ] ); 
+});
