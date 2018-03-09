885db2c2a7a83b2a3c7b3a72aab723f5e2558be3
Adam J. Piechnik <adam@piechnik.ca>
2018-03-09 10:48:47 -0800

    Get forms AJAX working WIP

--- src/js/site.js
+++ src/js/site.js
@@ -1,1 +1,35 @@
 console.log('Welcome to Aslan Ventures INC!');
+
+// FORM SCRIPT
+    // Set up an event listener for the contact form.
+    $('form').submit(function(e) {
+
+      e.preventDefault();
+
+      // Serialize the form data.
+      var formData = $('form').serialize();
+      var formMessages = $('#form-messages');
+
+      $.ajax({
+        type: "POST",
+        url: $('form').attr('action'),
+        data: formData,
+        success: function(){
+          $('form').removeClass('error');
+          $('form').addClass('success');
+          $(formMessages).addClass('success');
+          $(formMessages).removeClass('error');
+          $('form button').text('Successfully Sent!');
+          $('form').find('#name, #email, #phone, #message').val('');
+          console.log('success');
+        },
+        error: function(){
+          $('form').removeClass('success');
+          $('form').addClass('error');
+          $(formMessages).addClass('error');
+          $(formMessages).removeClass('success');
+          $('form button').text('Please Try Again');
+          $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+      });
+    });
